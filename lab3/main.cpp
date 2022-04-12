5d327dfe2ebe06c527476b71306fe9affa2d705d
thabinhl21 <binhle6345@gmail.com>
2022-04-12 00:55:44 -0700

    selection sort works

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -26,7 +26,43 @@ unsigned min_index(const vector<T> &vals, unsigned index) {
     return min;
 }
 
-int main() {
+template<typename T>
+void selection_sort(vector<T> &vals) {
+    unsigned int indexMin;
+    T temp;
 
+    for (unsigned int i = 0; i < vals.size(); ++i) {
+        indexMin = min_index(vals,i);
+        temp = vals.at(i);
+        vals.at(i) = vals.at(indexMin);
+        vals.at(indexMin) = temp;
+    }
+}
+
+vector<char> createVector(){
+    int vecSize = rand() % 26;
+    char c = 'a';
+    vector<char> vals;
+    for(int i = 0; i < vecSize; i++)
+    {
+        vals.push_back(c);
+        c++;
+    }
+    return vals;
+}
+
+
+int main() {
+    srand(time(0));
+    vector<char> vals = createVector();
+    char curChar;
+    int index;
+    int numOfRuns = 10;
+    while(--numOfRuns >= 0){
+        cout << "Enter a number: " << endl;
+        cin >> index;
+        curChar = getElement(vals,index);
+        cout << "Element located at " << index << ": is " << curChar << endl;
+}
     return 0;
 }
