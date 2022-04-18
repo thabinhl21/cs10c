6f7b0c375ccc64edf0289f9adf7bfbd3fefaecc1
mecha-heart <sam.capo.shin@gmail.com>
2022-04-17 22:59:32 -0700

    namespace added

--- lab4/stack.h
+++ lab4/stack.h
@@ -3,6 +3,8 @@
 
 #include <stdexcept>
 
+using namespace std;
+
 template<typename T>
 class stack {
     private:
@@ -17,7 +19,7 @@ class stack {
 
         void push(T val) {
             if (size > MAX_SIZE) {
-                throw overflow_error("Called push on full stack");
+                throw overflow_error("Called push on full stack.");
             }
             for (int i = 0; i < size; i++) {
                 data[i] = val;
@@ -26,24 +28,24 @@ class stack {
 
         void pop() {
             if (size == 0) {
-                throw out_of_range("Called pop on empty stack");
+                throw out_of_range("Called pop on empty stack.");
             }
             size -= 1;
         }
 
         void pop_two() {
             if (size == 1) {
-                throw out_of_range("Called pop_two on a stack of size 1");
+                throw out_of_range("Called pop_two on a stack of size 1.");
             }
             else if (size == 0) {
-                throw out_of_range("Called pop_two on empty stack");
+                throw out_of_range("Called pop_two on empty stack.");
             }
             size -= 2;
         }
 
         T top() {
             if (size == 0) {
-                throw underflow_error("Called top on empty stack");
+                throw underflow_error("Called top on empty stack.");
             }
             return data[size - 1];
         }
