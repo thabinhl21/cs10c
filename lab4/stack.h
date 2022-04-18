ca3d0e5400df74563eeb24e7cd3f2ba65a9fb437
thabinhl21 <binhle6345@gmail.com>
2022-04-17 21:37:59 -0700

    stack class

--- lab4/stack.h
+++ lab4/stack.h
@@ -1,20 +1,61 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
+#include <stdexcept>
+
 template<typename T>
-class Stack {
+class stack {
     private:
-        const int MAX_SIZE = 20;
+        static const int MAX_SIZE = 20;
         T data[MAX_SIZE];
         int size;
 
     public:
-        Stack();
-        void push(T val);
-        void pop();
-        void pop_two();
-        T top();
-        bool empty();
+        stack<T>() {
+            size = 0;
+        }
+
+        void push(T val) {
+            if (size > MAX_SIZE) {
+                throw overflow_error("Called push on full stack");
+            }
+            for (int i = 0; i < size; i++) {
+                data[i] = val;
+            }
+        }
+
+        void pop() {
+            if (size == 0) {
+                throw out_of_range("Called pop on empty stack");
+            }
+            size -= 1;
+        }
+
+        void pop_two() {
+            if (size == 1) {
+                throw out_of_range("Called pop_two on a stack of size 1");
+            }
+            else if (size == 0) {
+                throw out_of_range("Called pop_two on empty stack");
+            }
+            size -= 2;
+        }
+
+        T top() {
+            if (size == 0) {
+                throw underflow_error("Called top on empty stack");
+            }
+            return data[size - 1];
+        }
+
+        bool empty() {
+            if (size == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
 };
 
 #endif
