7ace43e40cfbc2e8e2de6a091ce44481ee2dee06
mecha-heart <sam.capo.shin@gmail.com>
2022-05-17 18:27:56 -0700

    lab8-2

--- program3/Node.h
+++ program3/Node.h
@@ -11,13 +11,15 @@ string data;
 
 int height;
 
+int count;
+
 Node* left;
 
 Node* right;
 
 Node* parent;
 
-Node(string data): data(data), height(0), left(nullptr), right(nullptr), parent(nullptr) {}
+Node(string data): data(data), height(0), count(0), left(nullptr), right(nullptr), parent(nullptr) {}
 };
 
 #endif
