31f0083e34052dc8199a216b62c139980aab1a1e
Binh Le <68206029+thabinhl21@users.noreply.github.com>
2022-05-24 20:29:44 -0700

    Add files via upload

---
+++ program4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    // get input from file
+
+    // as we are getting input, add each word into hash table checking if word already exists in table
+    // if it exists, increment the frequency
+
+    // after the whole file has been read, sort the frequencies of each word in descending order
+    // assign codes (1...) starting from the highest frequency
+
+    return 0;
+}
