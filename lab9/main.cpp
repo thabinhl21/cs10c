93b3d89eaed387154f80078a3ed0c171775a94dc
mecha-heart <sam.capo.shin@gmail.com>
2022-05-24 18:39:32 -0700

    median of three

--- lab9/main.cpp
+++ lab9/main.cpp
@@ -23,19 +23,70 @@ void fillArrays(int arr1[], int arr2[],int arr3[]){
     }
 }
 
-
 // this function sorts the given array in the range from i to k using quicksort method. In this 
 // function, pivot is the midpoint element (numbers[(i+k)/2])
 void Quicksort_midpoint(int numbers[], int i, int k) {
     return;
 }
 
-
 //this function utilizes different pivot selection technique in quicksort algorithm. The pivot is the median of 
 //the following three values: leftmost (numbers[i]), midpoint (numbers[(i+k)/2]) and rightmost (numbers[k]). 
 //You should modify the partition function given in section 21.5 to select the pivot using median-of-three technique.
 void Quicksort_medianOfThree(int numbers[], int i, int k) {
-    return;
+    int j;
+
+    if (i >= k)
+    {
+        return;
+    }
+
+    j = Partition_Three(numbers, i, k);
+
+    Quicksort_medianOfThree(numbers, i, j);
+    Quicksort_medianOfThree(numbers, j+1, k);
+}
+
+int Partition_Three(int numbers[], int i, int k)
+{
+    int l;
+    int h;
+    int midpoint;
+    int pivot;
+    int temp;
+    bool done;
+
+    midpoint = (i + ((i + k) / 2) + k) / 3;
+
+    pivot = numbers[midpoint];
+    done = false;
+    l = i;
+    h = k;
+    
+    while (!done)
+    {
+        while (numbers[l] < pivot)
+        {
+            ++l;
+        }
+        while (pivot < numbers[h])
+        {
+            --h;
+        }
+        if (l >= h)
+        {
+            done = true;
+        }
+        else
+        {
+            temp = numbers[l];
+            numbers[l] = numbers[h];
+            numbers[h] = temp;
+            ++l;
+            --h;
+        }
+
+    }
+    return h;
 }
 
 //this function sorts the given array using InsertionSort method. (implementation of this method is provided in section 21.3).
