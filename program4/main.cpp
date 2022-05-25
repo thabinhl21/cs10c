4245c7a0cfd34e76ed8640266a15da8de3a68eae
mecha-heart <sam.capo.shin@gmail.com>
2022-05-24 22:54:30 -0700

    prog4

--- program4/main.cpp
+++ program4/main.cpp
@@ -5,7 +5,19 @@ using namespace std;
 
 int main() {
 
+    //what does the decoder mapping file contains?
+    
     // get input from file
+
+    //store words in vector as WordEntry objects
+
+    //WordEntry class
+    //string word
+    //int frequency
+
+    //while adding - need logic for checking if word already exists and incrementing frequency if it does
+
+    //sort vector by frequency (descending order)
 
     // as we are getting input, add each word into hash table checking if word already exists in table
     // if it exists, increment the frequency
