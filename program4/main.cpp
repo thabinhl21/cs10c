7e18ec59b56bd326ce12164e7fe582d875d88ece
mecha-heart <sam.capo.shin@gmail.com>
2022-05-24 22:54:59 -0700

    prog4-2

--- program4/main.cpp
+++ program4/main.cpp
@@ -19,11 +19,6 @@ int main() {
 
     //sort vector by frequency (descending order)
 
-    // as we are getting input, add each word into hash table checking if word already exists in table
-    // if it exists, increment the frequency
-
-    // after the whole file has been read, sort the frequencies of each word in descending order
-    // assign codes (1...) starting from the highest frequency
 
     return 0;
 }
