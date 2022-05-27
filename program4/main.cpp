8b24d6a4691754171706def0f44c35016f670e55
thabinhl21 <binhle6345@gmail.com>
2022-05-27 11:03:36 -0700

    started getting input

--- program4/main.cpp
+++ program4/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 #include <fstream>
-
+#include "HashTable.h"
+#include "Token.h"
 using namespace std;
 
 int main() {    
     // get input from file
+    string filename;
+    ifstream inFS;
+    string word;
 
+    cout << "Enter the name of the your file: " << endl;
+    cin >> filename;
+
+    inFS.open(filename);
+
+    if(!inFS.is_open()) {
+        cout << "Error opening " << filename << endl;
+        exit(1);
+    }
+
+    while (inFS >> word) {
     //store words in hash table (vector) as WordEntry objects (HashTable::put - do not need to compute hash!)
+
+    }
 
     //WordEntry class
     //string word
